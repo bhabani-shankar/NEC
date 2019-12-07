da8010188d7547eff813ca9f8fb5cb657ff11d4e
Subhajitmohanty <subhamohanty56@gmail.com>
2019-12-08 00:33:55 +0530

    modified existing files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+
 import {
   Collapse,
   Navbar,
@@ -21,21 +22,33 @@ import {
   FormControl
 } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image'
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">
+  <img
+        alt=""
+        src="/logo.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"/>
+  HAPPY FACES</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link eventKey={2} href="#signup">
+        Sign Up
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 
 
-function App() {
-  return (
-    <Navbar bg="light" variant="light">
-    <Navbar.Brand href="#home">Happy Faces</Navbar.Brand>
-    <Navbar.Collapse className="justify-content-end">
-    <Nav.Link href="#home">About</Nav.Link>
-    <Nav.Link href="#home">Sign up</Nav.Link>
-    </Navbar.Collapse>
-    
-  </Navbar>
-  <Image src="" fluid />
-  
-  );
-}
 
 export default App;
